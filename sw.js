73d62657557d2ed17dec279655732fd1eaa88c39
Zayri Wilson <zayriazrielwilsonsanchez@gmail.com>
2026-05-27 16:27:37 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,27 @@
-const CACHE_NAME = 'forestal-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './css/bootstrap.min.css',
-  './js/calculadora.js',
-  './js/pwa-setup.js',
-  './manifest.json',
-  './icono-192.png'
+const CACHE_NAME = 'forestal-v2';
+const assets = [
+  'index.html',
+  'css/bootstrap.min.css',
+  'js/calculadora.js',
+  'js/pwa-setup.js',
+  'manifest.json',
+  'logo.svg'
 ];
 
+// Instalar y guardar archivos en caché
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
   );
 });
 
+// Responder desde la caché cuando no haya internet
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
